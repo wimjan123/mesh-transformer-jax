3a2863e04488e7f4c7007f37da4b76a8ddd77649
wimjan123 <visser.william@live.nl>
2023-03-08 20:09:54 +0100

    Update Resharding-4shards.py

--- Resharding-4shards.py
+++ Resharding-4shards.py
@@ -24,7 +24,7 @@ params = {
   "pe_rotary_dims": 64,
   "early_cast": True,
   "seq": 2048,
-  "cores_per_replica": 4,  # only running on one GPU
+  "cores_per_replica": 1,  # only running on one GPU
   "per_replica_batch": 1,
 }
 
@@ -38,7 +38,9 @@ params["sampler"] = nucleaus_sample
 params["optimizer"] = optax.scale(0)
 
 devices = np.array([jax.devices()[0]]).reshape((1, 1))
-maps.thread_resources.env = maps.ResourceEnv(maps.Mesh(devices, ('dp', 'mp')))
+mesh_shape = (4, 1)
+loops = 1  # only running on one GPU
+maps.thread_resources.env = maps.ResourceEnv(maps.Mesh(devices, mesh_shape, ('dp', 'mp'), loops))
 
 tokenizer = transformers.GPT2TokenizerFast.from_pretrained('gpt2')
 
@@ -65,7 +67,7 @@ network.state = np.stack(np.split(network.state, 4))
 output_data = io.BytesIO()
 np.savez_compressed(output_data, *network.state)
 output_data.seek(0)
-output_blob = bucket.blob('checkpoint/step_383500_sharded')
+output_blob = bucket.blob('checkpoint/step_383500_shardedv4')
 output_blob.upload_from_file(output_data)
 
 def infer(context, top_k=40, top_p=0.9, temp=1.0, gen_len=512):
